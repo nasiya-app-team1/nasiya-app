0c15dd35fda33540f973ac8f761e6fa9a1c17ade
mehriddin-amonboyev <mehriddinamonboyev1@gmail.com>
2025-01-22 00:02:55 +0500

    phone_number module create

---
+++ src/api/phone-numbers/phone-numbers.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { PhoneNumbersService } from './phone-numbers.service';
+import { PhoneNumbersController } from './phone-numbers.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PhoneNumber } from 'src/entities/phoneNumber.entity';
+
+@Module({
+  imports:[TypeOrmModule.forFeature([PhoneNumber])],
+  controllers: [PhoneNumbersController],
+  providers: [PhoneNumbersService],
+})
+export class PhoneNumbersModule {}
