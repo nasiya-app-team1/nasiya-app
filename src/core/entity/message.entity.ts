3c361c236ef7bc9fbe221200b5b93cebccdbb45f
abdulaz1z31 <a.abdulaziz3110@gmail.com>
2025-01-23 12:45:14 +0500

    resolved conflict

--- src/core/entity/message.entity.ts
+++ src/core/entity/message.entity.ts
@@ -1,5 +1,5 @@
-import { BaseEntity } from "src/common/database/baseEntity";
-import {Column, Entity } from "typeorm";
+import { BaseEntity } from 'src/common/database/baseEntity';
+import { Column, Entity } from 'typeorm';
 // export enum Message {
 //     SENT = 'sent',
 //     FAILED = 'failed',
@@ -8,18 +8,18 @@ import {Column, Entity } from "typeorm";
 
 @Entity('message')
 export class MessageEntity extends BaseEntity {
-    @Column({ type: 'varchar', name: 'store_id' })
-    store_id: string;
+  @Column({ type: 'varchar', name: 'store_id' })
+  store_id: string;
 
-    @Column({ type: 'varchar', name: 'debtor_id' })
-    debtor_id: string;
+  @Column({ type: 'varchar', name: 'debtor_id' })
+  debtor_id: string;
 
-    @Column({type: 'varchar', name: 'content'})
-    content: string;
+  @Column({ type: 'varchar', name: 'content' })
+  content: string;
 
-    // @Column({ type: 'enum', name: 'status' })
-    // status: Message;
+  // @Column({ type: 'enum', name: 'status' })
+  // status: Message;
 
-    @Column({ type: 'varchar', name: 'sample_message_id' })
-    sample_message_id: string;
+  @Column({ type: 'varchar', name: 'sample_message_id' })
+  sample_message_id: string;
 }
