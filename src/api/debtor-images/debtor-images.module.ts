9d0de448f45f9a53ec9dee812d19f890537f0e9d
abdulaz1z31 <a.abdulaziz3110@gmail.com>
2025-01-25 14:44:53 +0500

    deleted unnecessary files

--- src/api/debtor-images/debtor-images.module.ts
+++ src/api/debtor-images/debtor-images.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DebtorImagesService } from './debtor-images.service';
-import { DebtorImagesController } from './debtor-images.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DebtorImageEntity } from 'src/core/entity/debtor-image.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([DebtorImageEntity])],
-  controllers: [DebtorImagesController],
+  controllers: [],
   providers: [DebtorImagesService],
 })
 export class DebtorImagesModule {}
--- src/api/debts-images/debts-images.module.ts
+++ src/api/debts-images/debts-images.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DebtsImagesService } from './debts-images.service';
-import { DebtsImagesController } from './debts-images.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DebtImageEntity } from 'src/core/entity/debt-image.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([DebtImageEntity])],
-  controllers: [DebtsImagesController],
+  controllers: [],
   providers: [DebtsImagesService],
 })
 export class DebtsImagesModule {}
