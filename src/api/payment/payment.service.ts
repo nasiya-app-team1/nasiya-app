2da4026877367fe72c90de2f11cebb7371756d67
abdulaz1z31 <a.abdulaziz3110@gmail.com>
2025-01-30 01:00:19 +0500

    added statistics

--- src/api/payment/payment.service.ts
+++ src/api/payment/payment.service.ts
@@ -5,8 +5,7 @@ import { CreatePaymentDto } from './dto/create-payment.dto';
 import { UpdatePaymentDto } from './dto/update-payment.dto';
 import { PaymentEntity } from 'src/core/entity/payment.entity';
 import { BaseService } from 'src/infrastructure';
-import { PaymentRepository } from 'src/core';
-import { DebtsService } from '../debts/debts.service';
+import { DebtEntity, DebtsRepository, PaymentRepository } from 'src/core';
 
 @Injectable()
 export class PaymentService extends BaseService<
@@ -15,17 +14,38 @@ export class PaymentService extends BaseService<
 > {
   constructor(
     @InjectRepository(PaymentEntity) repository: PaymentRepository,
-    private readonly debtsService: DebtsService,
+    @InjectRepository(DebtEntity) private debtsRepository: DebtsRepository,
   ) {
     super(repository);
   }
 
   async createPayment(createPaymentDto: CreatePaymentDto) {
-    const debt = await this.debtsService.findOne(createPaymentDto.debt_id);
-
+    const debt = await this.debtsRepository.findOneBy({
+      id: createPaymentDto.debt_id,
+    });
     if (!debt) {
       throw new BadRequestException('Relation debt not found');
     }
+    const date = new Date(debt.debt_date);
+    date.setMonth(date.getMonth() + 1);
+    const newDate = date.toISOString().split('T')[0];
+    let month: number;
+    if (createPaymentDto.type == 'one_month') {
+      month = 1;
+    } else if (createPaymentDto.type == 'multi_month') {
+      const moneyForMonth = debt.debt_sum / debt.debt_period;
+      month = Math.floor(createPaymentDto.sum / moneyForMonth);
+    } else {
+      month = 1;
+    }
+    const newDebt = {
+      updated_at: new Date(Date.now()),
+      debt_date: newDate,
+      debt_sum: debt.debt_sum - createPaymentDto.sum,
+      debt_period: debt.debt_period - month,
+    };
+
+    await this.debtsRepository.update(debt.id, newDebt);
     return await this.create(createPaymentDto);
   }
 
@@ -41,7 +61,7 @@ export class PaymentService extends BaseService<
     const [payment, debt] = await Promise.all([
       this.findOneById(id),
       dto.debt_id
-        ? this.debtsService.findOne(dto.debt_id)
+        ? this.debtsRepository.findOneBy({ id: dto.debt_id })
         : Promise.resolve(null),
     ]);
 
